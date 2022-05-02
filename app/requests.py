ea9ef848da5272c656bb7919274994705da5a867
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-03 00:56:39 +0300

    feat:request.py - gets the news sources names from the API

---
+++ app/requests.py
@@ -0,0 +1,38 @@
+from app import app
+import urllib.request,json
+from .models import news
+
+News_Sources= news.News_Source
+def get_news():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = 'https://newsapi.org/v2/top-headlines/sources?apiKey=8bcc39f4104646c09fdd3c037f85a7ca'
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+
+
+    return news_results
+
+def process_results(news_list):
+    news_results = []
+    for news_item in news_list:
+        name =news_item.get('name')
+
+        if name:
+            news_object = News_Sources(name)
+            news_results.append(news_object)
+
+    return news_results
+
+
+
+  
+
