cf72f8dec12bad44db4e2d06ec75588761870a10
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-03 00:21:50 +0300

    updates the __init__.py: the brain of the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 
 from app.config import DevConfig, ProdConfig
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
 
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+
 
 from app import views
